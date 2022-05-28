0e644f1448a8d84052dd6525dc9fe49589cca27f
myeongjunPark <mjooon98@kakao.com>
2022-05-29 03:18:08 +0900

    App.js route path fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Route basename={process.env.PUBLIC_URL} path='/movie/:id' element={<Detail />} />
     </Routes>
     <Routes>
-      <Route /*path={`${process.env.PUBLIC_URL}/`}*/ path='/' element={<Home />}/>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}/>
     </Routes>
   </Router>
   )
