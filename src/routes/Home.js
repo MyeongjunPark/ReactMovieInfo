b4075b3ff565976a40adc46ca6f00c565bcc86f6
myeongjunPark <mjooon98@kakao.com>
2022-05-29 13:34:37 +0900

    console.log delete

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -46,7 +46,6 @@ function Home() {
     const response = await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
       const json = await response.json();
-      console.log(json.data.movies)
       setMovies(json.data.movies)
       setLoading(false)
   }
